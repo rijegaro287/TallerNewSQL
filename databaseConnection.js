12268a761e4bfccfee03ff2ecfe4e5081adab583
Ricardo Gatgens <gatgens48@gmail.com>
2022-11-02 15:19:26 -0600

    comentarios

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,15 +1,19 @@
-// Imports the Google Cloud client library
+// Importa la librería de Google Cloud Spanner
 const { Spanner } = require('@google-cloud/spanner')
 
+// Identificadores del proyecto, instancia y base de datos creadas
+// desde la consola de Google Cloud
 const projectId = 'tallernewsql'
 const instanceId = 'ejemplotaller'
 const databaseId = 'ejemplo_taller'
 
-// Creates a client
+// Se conecta con el cliente de autentificación de Google Cloud
 const spanner = new Spanner({ projectId: projectId })
 
-// Gets a reference to a Cloud Spanner instance and database
+// Crea una conexión directa con la instancia de Spanner
 const instance = spanner.instance(instanceId)
 const database = instance.database(databaseId)
 
+// Exporta la conexión para poder ser utilizada en otros módulos
 module.exports = { database }
+
